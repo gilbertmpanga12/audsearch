6cbe39900f6b797e57793557fb28e40c9e693468
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-02-27 19:15:29 +0300

    Update:replaced results page

---
+++ src/app/results/results.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResultsRoutingModule } from './results-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ResultsRoutingModule
+  ]
+})
+export class ResultsModule { }
--- src/app/top-audicles/top-audicles.module.ts
+++ src/app/top-audicles/top-audicles.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AudiclesComponent } from './audicles/audicles.component';
+import {Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [{
+  path: '',
+  component: AudiclesComponent
+  }];
 
 @NgModule({
   declarations: [AudiclesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class TopAudiclesModule { }
