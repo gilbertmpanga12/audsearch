d3dc78daf1ab46899a7fe0ef1aa27eb7ba0bb83c
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-02-27 18:12:10 +0300

    "Update: introduced font-awesome to project"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {RouterModule} from '@angular/router';
+import {routes} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
