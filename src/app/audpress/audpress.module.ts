e16b91b396ff17fea5bcd7cbdfd18ab8f627b67e
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-02-28 18:13:00 +0300

    Update: created audpress module

---
+++ src/app/audpress/audpress.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AudpressRoutingModule } from './audpress-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AudpressRoutingModule
+  ]
+})
+export class AudpressModule { }
