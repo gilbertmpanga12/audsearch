577efc5aac6fcbf4e1cd3419ced3b2336e4a4531
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-03-01 14:18:46 +0300

    "Update: configured the guard"

--- src/app/main.guard.ts
+++ src/app/main.guard.ts
@@ -2,13 +2,33 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import {map} from 'rxjs/operators';
+import {take} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
+import {MainService} from './main.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {Router} from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MainGuard implements CanActivate {
+  constructor(private service: MainService,
+    private af: AngularFireAuth, private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+      if (this.service.isAuthenticated) {
+        return true;
+      }
+      return this.af.authState.pipe(
+        take(1),
+        map(user => !!user),
+        tap(loggedIn => {
+            if (!loggedIn) {
+                this.router.navigate(['/auth']);
+            }
+        })
+      );
   }
 }
--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -6,12 +6,29 @@ import { auth } from 'firebase/app';
   providedIn: 'root'
 })
 export class MainService {
+  firebaseAuthState: any = null;
+  tokenResult: any;
+  firebaseId: any;
 
-  constructor(private af: AngularFireAuth) { }
+  constructor(private af: AngularFireAuth) {
+    this.af.authState.subscribe(user => {
+      if (user !== null) {
+        this.firebaseAuthState = user;
+        this.firebaseId = user.uid;
+        // this.email = user.email;
+        user.getIdTokenResult().then(result => {
+          this.tokenResult =  result.token;
+        });
+      }
+    });
+  }
   googleSignIn() {
     return this.af.auth.signInWithRedirect(new auth.GoogleAuthProvider());
   }
   facebookSignIn() {
     return this.af.auth.signInWithRedirect(new auth.FacebookAuthProvider());
+  }
+  get isAuthenticated() {
+    return this.firebaseAuthState !== null;
   }
 }
