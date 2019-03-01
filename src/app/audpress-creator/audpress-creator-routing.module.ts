59cd61448dabf19942261e03fe63b2b862fe3499
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-03-01 11:04:48 +0300

    "Update: configured new serie component"

--- src/app/audpress-creator/audpress-creator-routing.module.ts
+++ src/app/audpress-creator/audpress-creator-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AudpressCreatorComponent} from './audpress-creator/audpress-creator.component';
+import {NewSerieComponent} from './new-serie/new-serie.component';
 
 const routes: Routes = [{
 path: '',
 component: AudpressCreatorComponent
-
-}];
+},
+{
+  path: 'new-serie',
+  component: NewSerieComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
