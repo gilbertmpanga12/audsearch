bbd886b193449e46cfa91a20a8481b838bcdd593
gilbert mpanga <gilbertmpanga.gm@gmail.com>
2019-03-01 12:32:50 +0300

    "Update: add web set-up for app module"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC0dlmdNOwcfqTYvTkhYBKfAJmn5BQPFv8",
+    authDomain: "audsearch-web.firebaseapp.com",
+    databaseURL: "https://audsearch-web.firebaseio.com",
+    projectId: "audsearch-web",
+    storageBucket: "audsearch-web.appspot.com",
+    messagingSenderId: "920530303250"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC0dlmdNOwcfqTYvTkhYBKfAJmn5BQPFv8",
+    authDomain: "audsearch-web.firebaseapp.com",
+    databaseURL: "https://audsearch-web.firebaseio.com",
+    projectId: "audsearch-web",
+    storageBucket: "audsearch-web.appspot.com",
+    messagingSenderId: "920530303250"
+  }
 };
 
 /*
